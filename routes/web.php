feb7eed77fc552f7a607ddc8b5249da238ad082e
unknown <irineu.joel01@gmail.com>
2021-08-23 11:44:14 -0300

    conhecendo o blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $nome = 'irineu';
+    $idade = 20;
+
+    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => 'programador']);
 });
 
 Route::get('/contact', function () {
