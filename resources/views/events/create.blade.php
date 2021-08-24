8a9391caf30d4a3908e2bab1c66e9080d2943194
unknown <irineu.joel01@gmail.com>
2021-08-24 14:21:14 -0300

    salvando dados no mysql e conclusão da pagina de criar eventos

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -3,5 +3,30 @@
 @section('title', 'criar evento')
 
 @section('content')
-    <h1>Criar Evento</h1>
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie o seu evento</h1>
+        <form action="/events" method="POST">
+            @csrf
+            <div class="fomrgroup">
+               <label for="title">Evento:</label>
+               <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento"> 
+            </div>
+            <div class="fomrgroup">
+                <label for="title">Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento"> 
+             </div>
+             <div class="fomrgroup">
+                <label for="title">O evento é privado?</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+             </div>
+             <div class="fomrgroup">
+                <label for="title">Evento:</label>
+                <textarea name="description" id="description" placeholder="O que vai acontecer no evento?" class="form-control"></textarea>
+            </div>
+            <input type="submit" value="Criar Evento" class="btn btn-primary" id="btn-create">
+        </form>
+    </div>
 @endsection
