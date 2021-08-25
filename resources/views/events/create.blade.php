ec8210261889fbb6eabcbeb6b49b054810f5cf32
unknown <irineu.joel01@gmail.com>
2021-08-25 13:39:35 -0300

    salvando json/arrays no banco

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -18,18 +18,37 @@
             <div class="fomrgroup">
                 <label for="title">Cidade:</label>
                 <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento"> 
-             </div>
-             <div class="fomrgroup">
+            </div>
+            <div class="fomrgroup">
                 <label for="title">O evento é privado?</label>
                 <select name="private" id="private" class="form-control">
                     <option value="0">Não</option>
                     <option value="1">Sim</option>
                 </select>
-             </div>
-             <div class="fomrgroup">
-                <label for="title">Evento:</label>
+            </div>
+            <div class="fomrgroup">
+                <label for="title">Descrição:</label>
                 <textarea name="description" id="description" placeholder="O que vai acontecer no evento?" class="form-control"></textarea>
             </div>
+            <div class="fomrgroup">
+                <label for="title">Adicione, itens de infra-estrutura:</label>
+                <div class="form-group">
+                    <input type="checkbox" name="itens[]" value="Cadeiras"> Cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="itens[]" value="Palco"> Palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="itens[]" value="Bebida gratis"> Bebida gratis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="itens[]" value="Open food"> Open food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="itens[]" value="Brindes"> Brindes
+                </div>
+            </div>
+
             <input type="submit" value="Criar Evento" class="btn btn-primary" id="btn-create">
         </form>
     </div>
