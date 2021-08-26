135d18c653362f30a4dda58e0a96fd182be856dd
unknown <irineu.joel01@gmail.com>
2021-08-26 14:10:06 -0300

    configurando search/busca

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,17 @@
 @section('content')
     <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
-        <form action="">
+        <form action="/" method="GET">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
         </form>
     </div>
     <div id="events-container" class="col-md-12">
-        <h2>Proximos Eventos</h2>
-        <p>Veja os eventos dos proximos dias</p>
+        @if($search)
+            <h2>Resultados para: {{$search}}</h2>
+        @else
+            <h2>Proximos Eventos</h2>
+            <p>Veja os eventos dos proximos dias</p>
+        @endif
         <div id="cards-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
@@ -24,7 +28,9 @@
                     </div>
                 </div>
             @endforeach
-            @if (count($events) == 0)
+            @if(count($events) == 0&& $search)
+                <p>Não foi possivel encontrar nenhum evento com {{$search}}, <a href="/">Ver todos</a>.</p>
+            @elseif(count($events) == 0)
                 <p>Não há eventos disponiveis</p>
             @endif
         </div>
